71f8760ff3243eb58813ec6674beefe8e2b1cd2b
viviancan <vcnales@gmail.com>
2020-10-15 08:58:31 -0500

    3 or 5 multiples

---
+++ src/ThreeOrFiveMultiples.java
@@ -0,0 +1,23 @@
+/*
+* If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+* Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+* Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them)
+* Courtesy of projecteuler.net
+* */
+
+public class ThreeOrFiveMultiples {
+    public static void main(String[] args) {
+        System.out.println(solution(23));
+    }
+
+    public static int solution(int number) {
+        int bucket = 0;
+
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                bucket += i;
+            }
+        }
+        return bucket;
+    }
+}
