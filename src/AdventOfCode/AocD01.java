6d079d761ffdfe13464e248eeaee5d9c8c81976c
viviancan <vcnales@gmail.com>
2020-12-01 22:09:04 -0600

    updated solution

--- src/AdventOfCode/AocD01.java
+++ src/AdventOfCode/AocD01.java
@@ -14,71 +14,62 @@ public class AocD01 {
         System.out.println("Advent of Code 2020");
         System.out.println("********************");
 
+        ArrayList<Integer> numArray = new ArrayList<>();
+
         try{
             Scanner scanner = new Scanner(new FileReader("input.txt"));
 
-            ArrayList<Integer> numArray = new ArrayList<Integer>();
-
             while(scanner.hasNextInt()){
                 numArray.add(scanner.nextInt());
-            }
-
-            HashSet<Integer> numberSet = new HashSet<>();
-
-            for (int currentNum : numArray) {
-                //find the corresponding number that matches up wit the current number
-                int missingNum = 2020 - currentNum;
-
-                //check if the number is in the set
-                if (numberSet.contains(missingNum)) {
-                    System.out.printf("%nFinal Answer: %d%n", (currentNum * missingNum));
-                }
-                numberSet.add(currentNum);
             }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+        System.out.println("\nFinal Answer: " + partOne(numArray));
+        System.out.println("\nFinal Answer: " + partTwo(numArray));
 
-//        int finalResult = 0;
+    }
+
+    public static int partOne(ArrayList<Integer> numArray){
+        int finalAns = 0;
+        HashSet<Integer> numberSet = new HashSet<>();
+
+        for (int currentNum : numArray) {
+            //find the corresponding number that matches up wit the current number
+            int missingNum = 2020 - currentNum;
+
+            //check if the number is in the set
+            if (numberSet.contains(missingNum)) {
+                finalAns = currentNum * missingNum;
+            }
+            numberSet.add(currentNum);
+        }
+
+        return finalAns;
+
+    }
 
 
-//        for (int i = 0; i < numbers.length ; i += 1) {
-//
-//            for (int x = i + 1; x < numbers.length; x += 1) {
-//                int numA = numbers[i];
-//                int numB = numbers[x];
-//                int total = numA + numB;
-//
-//                if (total == 2020) {
-//                    finalResult = numA * numB;
-//                    break;
-//                }
-//            }
-//            if(finalResult >0){
-//                System.out.println(finalResult);
-//                break;
-//            }
-//        }
-//
-//        for (int i = 0; i < numbers.length-2 ; i += 1) {
-//            for (int x = i + 1; x < numbers.length-1; x += 1) {
-//                for(int y=x+1; y <numbers.length; y +=1){
-//                    int numA = numbers[i];
-//                    int numB = numbers[x];
-//                    int numC = numbers[y];
-//                    int total = numA + numB + numC;
-//
-//                    if (total == 2020) {
-//                        finalResult = numA * numB * numC;
-//                        System.out.println(finalResult);
-//                        System.exit(0);
-//
-//                    }
-//                }
-//            }
-//        }
+    public static int partTwo(ArrayList<Integer> numArray){
+        int finalAns = 0;
+
+        HashSet<Integer> numberSet = new HashSet<>();
+
+        for (int currentNum : numArray) {
+            for(int currentNum2 : numArray){
+                int missingNum = 2020 - currentNum - currentNum2;
+
+                if (numberSet.contains(missingNum)) {
+                    finalAns = currentNum * currentNum2 * missingNum;
+                }
+            }
+            numberSet.add(currentNum);
+        }
+
+        return finalAns;
+
     }
 }
 
